98a775fea156b9e3d2aa5f8c746a95a34b414c4a
sigristarisa <arisasigrist.ch@gmail.com>
2022-04-19 18:14:16 +0200

    added comments

--- src/index.js
+++ src/index.js
@@ -1,41 +1,46 @@
-console.log(data[0])
-const cards = document.querySelector(".cards")
+console.log(data);
+const cards = document.querySelector(".cards");
 
-for(let i = 0; i < data.length; i++) {
-    const card = document.createElement("li")
-    card.setAttribute("class", "card")
+for (let i = 0; i < data.length; i++) {
+  // create cards
+  const card = document.createElement("li");
+  card.setAttribute("class", "card");
 
-    const pokemonName = document.createElement("h2")
-    pokemonName.setAttribute("class", "card--title")
-    pokemonName.innerText = data[i].name[0].toUpperCase() + data[i].name.slice(1)
+  // create h2 pokemon names
+  const pokemonName = document.createElement("h2");
+  pokemonName.setAttribute("class", "card--title");
+  pokemonName.innerText = data[i].name[0].toUpperCase() + data[i].name.slice(1);
 
-    const img = document.createElement("img")
-    img.setAttribute("class", "card--img")
-    img.setAttribute("width", 256)
-    img.src = data[i].sprites.other["official-artwork"]["front_default"]
+  // create the image
+  const img = document.createElement("img");
+  img.setAttribute("class", "card--img");
+  img.setAttribute("width", 256);
+  img.src = data[i].sprites.other["official-artwork"]["front_default"];
 
-    const cardTexts = document.createElement("ul")
-    cardTexts.setAttribute("class", "card--text")
+  // create the texts
+  const cardTexts = document.createElement("ul");
+  cardTexts.setAttribute("class", "card--text");
 
-    for(let j = 0; j < 6; j++) {
-        const text = document.createElement("li")
-        text.innerText = `${data[i].stats[j].stat.name.toUpperCase()} : ${data[i].stats[j]["base_stat"]}`
-        cardTexts.appendChild(text)
-    }
+  for (let j = 0; j < 6; j++) {
+    const text = document.createElement("li");
+    text.innerText = `${data[i].stats[j].stat.name.toUpperCase()} : ${
+      data[i].stats[j]["base_stat"]
+    }`;
+    cardTexts.append(text);
+  }
 
-    const versionTexts = document.createElement("ul")
-    versionTexts.innerText = "APPEARED IN :"
-    versionTexts.setAttribute("class", "card--text")
-    for(let k = 0; k < data[i]["game_indices"].length; k++) {
-       const text = document.createElement("li")
-       text.innerText = data[i]["game_indices"][k].version.name
-       versionTexts.appendChild(text)
-    }
+  // create the texts of in which versions it appeared
+  const versionTexts = document.createElement("div");
+  versionTexts.innerText = "APPEARED IN :";
+  versionTexts.setAttribute("class", "card--text");
 
-    card.append(pokemonName)
-    card.append(img)
-    card.append(cardTexts)
-    card.append(versionTexts)
+  const text = document.createElement("p");
+  for (let k = 0; k < data[i]["game_indices"].length; k++) {
+    text.innerText += `${data[i]["game_indices"][k].version.name}, `;
+  }
+  versionTexts.append(text);
 
-    cards.appendChild(card)
+  // append elements
+  card.append(pokemonName, img, cardTexts, versionTexts);
+  cards.append(card);
 }
