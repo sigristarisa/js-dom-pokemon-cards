a766be2c32844eba8ab8c8f4e990a20498cc20a4
sigristarisa <arisasigrist.ch@gmail.com>
2022-04-19 16:20:37 +0200

    finished the basic structure

--- src/index.js
+++ src/index.js
@@ -4,29 +4,31 @@ console.log(data[0]);
 
 const cards = document.querySelector(".cards")
 
-const card = document.createElement("li")
-card.setAttribute("class", "card")
+for(let i = 0; i < data.length; i++) {
+    const card = document.createElement("li")
+    card.setAttribute("class", "card")
 
-const pokemonName = document.createElement("h2")
-pokemonName.setAttribute("class", "card--title")
-pokemonName.innerText = data[0].name
+    const pokemonName = document.createElement("h2")
+    pokemonName.setAttribute("class", "card--title")
+    pokemonName.innerText = data[i].name
 
-const img = document.createElement("img")
-img.setAttribute("class", "card--img")
-img.setAttribute("width", 256)
-img.src = data[0].sprites.other["official-artwork"]["front_default"]
+    const img = document.createElement("img")
+    img.setAttribute("class", "card--img")
+    img.setAttribute("width", 256)
+    img.src = data[i].sprites.other["official-artwork"]["front_default"]
 
-const cardTexts = document.createElement("ul")
-cardTexts.setAttribute("class", "card--text")
+    const cardTexts = document.createElement("ul")
+    cardTexts.setAttribute("class", "card--text")
 
-for(let i = 0; i < 6; i++) {
-    const text = document.createElement("li")
-    text.innerText = data[0].stats[i].stat.name + " : " + data[0].stats[i]["base_stat"]
-    cardTexts.appendChild(text)
+    for(let j = 0; j < 6; j++) {
+        const text = document.createElement("li")
+        text.innerText = data[i].stats[j].stat.name + " : " + data[i].stats[j]["base_stat"]
+        cardTexts.appendChild(text)
+    }
+
+    card.append(pokemonName)
+    card.append(img)
+    card.append(cardTexts)
+
+    cards.appendChild(card)
 }
-
-card.append(pokemonName)
-card.append(img)
-card.append(cardTexts)
-
-cards.appendChild(card)
